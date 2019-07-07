ace09aa70bcf3690657fb42a068e9f7c44d8c62a
m-m-e <megmyfedwards@gmail.com>
2019-07-07 17:53:44 +0200

    Add menu styles and change button image on click

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
 'use strict';
 
 const header = document.querySelector('.page__header');
+const nav = document.querySelector('.nav-active');
 
-const addMenuButton = () => {
-  const newContainer = document.createElement('div');
-  newContainer.classList.add('nav__trigger-container');
-
-  const newButton = document.createElement('img');
-  newButton.classList.add('nav__trigger-image');
-  
-  newContainer.appendChild(newButton);
-  header.appendChild(newContainer);
+const changeButtonImage = event => {
+  const target = event.currentTarget;
+  const classes = target.childNodes[0].classList;
+  if (classes[0] ==='nav__button-trigger'){
+    classes.remove('nav__button-trigger');
+    classes.add('nav__button-close');
+  } else {
+    classes.remove('nav__button-close');
+    classes.add('nav__button-trigger');
+  }
 }
 
-addMenuButton();
+const addMenuButton = container => {
+  const newContainer = document.createElement('div');
+  newContainer.classList.add('nav__button-container');
+  newContainer.addEventListener('click', changeButtonImage);
+
+  const newButton = document.createElement('img');
+  newButton.classList.add('nav__button-trigger');
+  
+  newContainer.appendChild(newButton);
+  container.appendChild(newContainer);
+}
+
+
+addMenuButton(header);
