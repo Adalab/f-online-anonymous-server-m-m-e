333801870804a894a9d57d251b036ad8a8b6ea10
m-m-e <megmyfedwards@gmail.com>
2019-07-07 18:44:07 +0200

    Simplify click function and classes

--- js/main.js
+++ js/main.js
@@ -1,31 +1,13 @@
 'use strict';
 
-const header = document.querySelector('.page__header');
-const nav = document.querySelector('.nav-active');
-
-const changeButtonImage = event => {
-  const target = event.currentTarget;
-  const classes = target.childNodes[0].classList;
-  if (classes[0] ==='nav__button-trigger'){
-    classes.remove('nav__button-trigger');
-    classes.add('nav__button-close');
-  } else {
-    classes.remove('nav__button-close');
-    classes.add('nav__button-trigger');
-  }
-}
-
-const addMenuButton = container => {
-  const newContainer = document.createElement('div');
-  newContainer.classList.add('nav__button-container');
-  newContainer.addEventListener('click', changeButtonImage);
-
-  const newButton = document.createElement('img');
-  newButton.classList.add('nav__button-trigger');
-  
-  newContainer.appendChild(newButton);
-  container.appendChild(newContainer);
-}
+const menuTrigger = document.querySelector('.nav-trigger');
+const menuClose = document.querySelector('.nav-close');
+const menu = document.querySelector('.nav');
 
 
-addMenuButton(header);
+const handleMenu = () => {
+  menu.classList.toggle('active');
+} 
+
+menuTrigger.addEventListener('click', handleMenu);
+menuClose.addEventListener('click', handleMenu);
